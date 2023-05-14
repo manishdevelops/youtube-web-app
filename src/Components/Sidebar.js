2c55d4b78c85082eaa61e128bc14432200ea94f2
manish mandal <76776194mm@gmail.com>
2023-05-14 22:53:45 +0530

    complete html & css

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,7 +1,63 @@
+import 'boxicons';
+
 import React from 'react';
 
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div className='p-5 shadow-lg w-52'>
+			<h1 className='font-bold pt-5'>Subscriptions</h1>
+			<ul>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-music'></i> <span className='mx-3'>Music</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bxs-medal'></i> <span className='mx-3'>Sports</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-game'></i>
+					<span className='mx-3'>Gaming</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-film'></i>
+					<span className='mx-3'>Movies</span>
+				</li>
+			</ul>
+			<hr />
+			<h1 className='font-bold pt-5'>Explore</h1>
+			<ul>
+				<li className='my-1 py-1.5'>
+					<i class='bx bxs-hot'></i> <span className='mx-3'>Trending</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-shopping-bag'></i>{' '}
+					<span className='mx-3'>Shopping</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-music'></i> <span className='mx-3'> Music</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-film'></i>{' '}
+					<span className='mx-3'>Movies & Shows</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-wifi'></i> <span className='mx-3'>Live</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-game'></i>
+					<span className='mx-3'>Gaming</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-news'></i> <span className='mx-3'>News</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bxs-medal'></i> <span className='mx-3'>Sports</span>
+				</li>
+				<li className='my-1 py-1.5'>
+					<i class='bx bx-bulb'></i> <span className='mx-3'>Learning</span>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Sidebar;
