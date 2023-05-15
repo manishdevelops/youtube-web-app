00f9727e5ae4f088ac02102f8fc9c38f0ef0f909
manish mandal <76776194mm@gmail.com>
2023-05-15 23:14:29 +0530

    add button list

--- src/Components/ButtonList.js
+++ src/Components/ButtonList.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Button from './Button';
+
+const btnList = [
+	'all',
+	'Gaming',
+	'Songs',
+	'Live',
+	'Soccer',
+	'Cricket',
+	'Cooking',
+	'Valentine',
+	'Shows',
+];
 
 const ButtonList = () => {
-	return <div>ButtonList</div>;
+	return (
+		<div className='flex'>
+			{btnList.map((btn, i) => (
+				<Button key={i} name={btn} />
+			))}
+		</div>
+	);
 };
 
 export default ButtonList;
