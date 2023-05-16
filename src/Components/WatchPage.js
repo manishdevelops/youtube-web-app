dc0112ad8556e6d64d63a8ddee7c2ddfcbfd5d88
manish mandal <76776194mm@gmail.com>
2023-05-16 15:34:20 +0530

    add video

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { closeMenu } from '../utils/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
-	return <div>WatchPage</div>;
+	const [searchParams] = useSearchParams();
+	console.log(searchParams.get('v'));
+
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(closeMenu());
+	});
+	return (
+		<div className='px-5'>
+			<iframe
+				width='1200'
+				height='600'
+				src={
+					'https://www.youtube.com/embed/' +
+					searchParams.get('v') +
+					'?autoplay=1'
+				}
+				title='YouTube video player'
+				frameBorder='0'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			></iframe>
+		</div>
+	);
 };
 
 export default WatchPage;
